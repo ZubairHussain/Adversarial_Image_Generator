316861efb3ca809b0890d0f1d9474235734d5007
ZubairHussain <zubairhussain918@hotmail.com>
2025-01-19 18:03:59 +0000

    Implemented pytests for testing the pipeline

---
+++ tests/test_adversarial_image_generator.py
@@ -0,0 +1,37 @@
+import pytest
+import os
+from PIL import Image
+import torch
+from adversarial_generator.generator import AdversarialImageGenerator
+from adversarial_generator.utils.image_utils import preprocess_image
+from adversarial_generator.utils.model_utils import load_model
+
+
+def test_adversarial_image_generator():
+
+    input_image_path = 'data/ImageNet_samples/n02124075_Egyptian_cat.jpeg'
+    output_image_path = 'output/Egyptian_cat_to_Tiger_cat.png'
+    target_class = 282 #  Tiger cat
+    epsilon = 0.05
+
+    # Initialize the generator
+    generator = AdversarialImageGenerator(model_name="resnet50", epsilon=epsilon)
+
+    # Generate the adversarial image
+    adv_tensor = generator.generate(
+        image_path=input_image_path,
+        target_class=target_class,
+        output_path=output_image_path,
+    )
+
+    # Check if output file exists
+    assert os.path.exists(output_image_path), "Output adversarial image not saved"
+
+    # Predict the class of the adversarial image
+    predicted_class = generator.predict(preprocess_image(output_image_path))
+
+    # Assert that the predicted class matches the target class
+    assert predicted_class == target_class, (
+        f"Adversarial image was classified as {predicted_class}, "
+        f"but the target class was {target_class}"
+    )
